313f520d791b81a2f5744f58310e27897f7d846a
Fahad IF <68955027+fahadif@users.noreply.github.com>
2022-11-05 21:36:36 +0600

    Create Count Characters.js

---
+++ Count Characters.js
@@ -0,0 +1,5 @@
+var message = prompt ("Please write your message here:");
+
+var messageCount = message.length;
+
+alert("You have written " + messageCount + " characters. You have " + (10 - messageCount) + " characters remaining.");
