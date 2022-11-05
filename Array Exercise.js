30385863b8ad98f8608cf713126572d2a4ab876b
Fahad IF <68955027+fahadif@users.noreply.github.com>
2022-11-05 21:28:58 +0600

    Create Array Exercise.js

---
+++ Array Exercise.js
@@ -0,0 +1,8 @@
+var guestList = ["Angel","Jacks","Pames","James","Laras","Jason"];
+
+console.log("There are " + guestList.length + " guests in total. They are: ")
+console.log(guestList);
+
+console.log("The fourth guest is:" + guestList[3]);
+
+guestList.includes("Jacks");
